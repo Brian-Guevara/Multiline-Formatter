cd44c792f988a5a0be62ab98118013a3f39a09e2
Brian-Guevara <90436141+Brian-Guevara@users.noreply.github.com>
2022-07-10 18:36:55 -0700

    Add project files.

---
+++ Models/FormatProcedure.cs
@@ -0,0 +1,118 @@
+﻿namespace MultiLineStringFormatter.Models
+{
+    using System;
+    using System.ComponentModel;
+    using System.IO;
+
+    public class FormatProcedure : INotifyPropertyChanged
+    {
+
+        public FormatProcedure()
+        {
+            Input = "Example";
+            Format = "'_DATA_'";
+            Delimiter = ",";
+        }
+
+
+        private String _Input;
+
+        public String Input
+        {
+            get { return _Input; }
+            set { 
+                _Input = value;
+                OnPropertyChanged("Input");
+            }
+        }
+
+        private String _Format;
+
+        public String Format 
+        {
+            get { return _Format; }
+            set { 
+                _Format= value;
+                OnPropertyChanged("Format");
+            
+            }
+        }
+
+
+        private String _Delimiter;
+
+        public String Delimiter
+        {
+            get { return _Delimiter; }
+            set { 
+                _Delimiter = value;
+                OnPropertyChanged("Delimter");
+
+            }
+        }
+
+
+        private String _Output;
+
+        public String Output
+        {
+            get { return _Output; }
+            set { 
+                _Output = value;
+                OnPropertyChanged("Output");
+            
+            }
+        }
+
+
+        public void formatInput()
+        {
+            Output = "";
+            String [] array = Input.Split("\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            int count = 1;
+            foreach (String item in array)
+            {
+                Output += Format.Replace("_DATA_", item).Replace("_#_",count.ToString()) + Delimiter + '\n';
+                count++;
+            }
+            int delimLength = Delimiter.Length + 1;
+            Output = Output.Remove(Output.Length - delimLength);
+            
+
+        }
+        
+        public void addData()
+        {
+            Format += "_DATA_";
+
+        }
+
+        public void addNumbering()
+        {
+            Format += "_#_";
+
+        }
+
+
+
+        #region INofityPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged (String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+
+            }
+
+
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,65 @@
+﻿using GalaSoft.MvvmLight.Command;
+using MultiLineStringFormatter.Models;
+using System.Windows.Input;
+
+namespace MultiLineStringFormatter.ViewModels
+{
+    internal class MainWindowViewModel
+    {
+        public MainWindowViewModel()
+        {
+            _Format = new FormatProcedure();
+        }
+
+        private FormatProcedure _Format;
+
+        public FormatProcedure Format
+        {
+            get { return _Format; }
+
+        }
+
+        public ICommand UpdateOutput
+        {
+            get
+            {
+                return new RelayCommand(formatText, () => true);
+            }
+        }
+
+        public void formatText()
+        {
+            Format.formatInput();
+
+        }
+
+        public ICommand AddData
+        {
+            get
+            {
+                return new RelayCommand(_AddData, () => true);
+            }
+        }
+
+        public void _AddData()
+        {
+            Format.addData();
+
+        }
+
+        public ICommand AddNumbering
+        {
+            get
+            {
+                return new RelayCommand(_AddNumbering, () => true);
+            }
+        }
+
+        public void _AddNumbering()
+        {
+            Format.addNumbering();
+
+        }
+
+    }
+}
